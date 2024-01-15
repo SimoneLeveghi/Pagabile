1599002e2dce835cc1a23ea190690d29d3e27a02
simone.leveghi <simone.leveghi@buonarroti.tn.it>
2024-01-15 09:15:45 +0100

    Implement Pagabile's methods

--- src/Prodotto.java
+++ src/Prodotto.java
@@ -4,4 +4,22 @@ public class Prodotto implements Pagabile {
     public Prodotto(float costo) throws Exception {
         if(costo <= 0) throw new Exception("Costo invalido");
     }
+
+
+    @Override
+    public double calcolaPrezzo(String modalita) {
+        if(modalita.trim().equalsIgnoreCase("carta")) {
+            return costo + 0.5;
+        }
+        else if(modalita.trim().equalsIgnoreCase("contanti")) {
+            return costo - (costo * 5/100);
+        }
+
+        return costo + 1;
+    }
+
+    @Override
+    public double effettuaPagamento(String modalita, double saldo) {
+        return saldo - calcolaPrezzo(modalita);
+    }
 }
--- src/Servizio.java
+++ src/Servizio.java
@@ -4,4 +4,25 @@ public class Servizio implements Pagabile {
     public Servizio(float costo) throws Exception {
         if(costo <= 0) throw new Exception("Costo invalido");
     }
+
+    @Override
+    public double calcolaPrezzo(String modalita) {
+        double prezzo = 0;
+        if(modalita.trim().equalsIgnoreCase("carta")) {
+            prezzo = costo + 0.5;
+        }
+        else if(modalita.trim().equalsIgnoreCase("contanti")) {
+            prezzo = costo - (costo * 5/100);
+        }
+        else {
+            prezzo = costo + 1;
+        }
+
+        return prezzo + (prezzo * 10/100);
+    }
+
+    @Override
+    public double effettuaPagamento(String modalita, double saldo) {
+        return 0;
+    }
 }
