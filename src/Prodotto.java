46568bf974d199d3117234d84c97536227c3448d
simone.leveghi <simone.leveghi@buonarroti.tn.it>
2024-01-15 09:03:45 +0100

    Add constructors

--- src/Prodotto.java
+++ src/Prodotto.java
@@ -1,2 +1,7 @@
 public class Prodotto implements Pagabile {
+    private float costo;
+
+    public Prodotto(float costo) throws Exception {
+        if(costo <= 0) throw new Exception("Costo invalido");
+    }
 }
--- src/Servizio.java
+++ src/Servizio.java
@@ -1,2 +1,7 @@
 public class Servizio implements Pagabile {
+    private float costo;
+
+    public Servizio(float costo) throws Exception {
+        if(costo <= 0) throw new Exception("Costo invalido");
+    }
 }
